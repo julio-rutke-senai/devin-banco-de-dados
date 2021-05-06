8a242c939b6f167730df1428fb56272aaa2b7cd2
Julio Cezar Rutke <julio.rutke@edu.sc.senai.br>
2021-05-06 18:29:22 -0300

    Ajustes de arquivos

--- aula_jdbc/src/Gerenciamento.java
+++ aula_jdbc/src/Gerenciamento.java
@@ -1,5 +1,3 @@
-package aula_bd;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
--- aula_jdbc/src/Main.java
+++ aula_jdbc/src/Main.java
@@ -1,5 +1,3 @@
-package aula_bd;
-
 public class Main {
 
 	public static void main(String[] args) {
